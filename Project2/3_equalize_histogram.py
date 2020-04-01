36b7f43f38dbae1387dfd67687f94247f9000c18
Xiejiadong <1934784736@qq.com>
2020-04-01 11:29:38 +0800

    + 代码注释

--- Project2/3_equalize_histogram.py
+++ Project2/3_equalize_histogram.py
@@ -1,18 +1,24 @@
 # coding=utf-8
 import cv2
 
+#灰度图直方图均衡化
 def equalizeHistogram(input, output):
 	img = cv2.imread(input, 0)
+	#对输入灰度图直方图均衡化
 	equ = cv2.equalizeHist(img)
+	#保存均衡化后的结果
 	cv2.imwrite(output, equ, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
+#彩色图直方图均衡化
 def equalizeHistogramRGB(input, output):
 	img = cv2.imread(input)
+	#读取图像并转换为YUV色彩空间
 	ycrgb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
 	channels = cv2.split(ycrgb)
 	cv2.equalizeHist(channels[0], channels[0])
 	cv2.merge(channels, ycrgb)
 	cv2.cvtColor(ycrgb, cv2.COLOR_YCR_CB2BGR, img)
+	#保存均衡化后的结果
 	cv2.imwrite(output, img, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
 equalizeHistogram('input/3_input.jpg', 'output/3_equalize_hisyogram.jpg')
--- Project2/draw_histogram.py
+++ Project2/draw_histogram.py
@@ -1,14 +1,18 @@
+# encoding:utf-8
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
+#drawHistogram 函数用来画直方图
 def drawHistogram(input, output, title = ''):
+	#导入图像
 	img = Image.open(input)
+	#将图像转换成灰度图像
 	img = img.convert('L')
+	#统计每种灰度出现的次数
 	arr = np.array(img).flatten()
-	# for i in range(len(arr)):
-	# 	arr[i] = arr[i] / (len(arr) / 256)
 	print(len(arr))
+	#绘制直方图，设置 x/y 轴相关参数和图标标题
 	plt.hist(arr, bins=256, facecolor='black', alpha=0.75, range=[0, 256])
 	plt.xlabel('Gray Value')
 	plt.ylabel('Number')
@@ -16,6 +20,7 @@ def drawHistogram(input, output, title = ''):
 	plt.savefig(output)
 	plt.show()
 
+#分别绘制处理前后的直方图
 drawHistogram('input/3_input.jpg', 'output/3_before.jpg', "Histogram Figure - Before")
 drawHistogram('output/3_equalize_histogram.jpg', 'output/3_after.jpg', "Histogram Figure - After")
 
